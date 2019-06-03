23b34a53fd027150946d5cf91bd277300dbb13e6
DIEGO ALEJANDRO ZAPATA VANEGAS <diegozapata176738@correo.itm.edu.co>
2019-06-03 02:41:12 -0500

    Register validations

--- Event.Common/Interfaces/IDialogService.cs
+++ Event.Common/Interfaces/IDialogService.cs
@@ -1,7 +1,23 @@
-﻿namespace Event.Common.Interfaces
+﻿using System;
+
+namespace Event.Common.Interfaces
 {
     public interface IDialogService
     {
         void Alert(string message, string title,  string okbtnText);
+
+        void Alert(
+            string message,
+            string title,
+            string okbtnText,
+            Action confirmed);
+
+        void Confirm(
+            string title,
+            string message,
+            string okButtonTitle,
+            string dismissButtonTitle,
+            Action confirmed,
+            Action dismissed);
     }
 }
--- Event.UICross.Android/Services/DialogService.cs
+++ Event.UICross.Android/Services/DialogService.cs
@@ -4,6 +4,7 @@
     using global::Android.App;
     using MvvmCross;
     using MvvmCross.Platforms.Android;
+    using System;
 
     public class DialogService : IDialogService
     {
@@ -16,8 +17,57 @@
             var adb = new AlertDialog.Builder(act);
             adb.SetTitle(title);
             adb.SetMessage(message);
-            adb.SetPositiveButton(okbtnText, (sender, args) => { /* some logic */ });
+            adb.SetPositiveButton(okbtnText, (sender, args) => { });
             adb.Create().Show();
+        }
+
+        public void Alert(string title, string message, string okbtnText, Action confirmed)
+        {
+            var top = Mvx.Resolve<IMvxAndroidCurrentTopActivity>();
+            var act = top.Activity;
+
+            var adb = new AlertDialog.Builder(act);
+            adb.SetTitle(title);
+            adb.SetMessage(message);
+            adb.SetPositiveButton(okbtnText, (sender, args) =>
+            {
+                if (confirmed != null)
+                {
+                    confirmed.Invoke();
+                }
+            });
+
+            adb.Create().Show();
+        }
+
+        public void Confirm(
+            string title,
+            string message,
+            string okButtonTitle,
+            string dismissButtonTitle,
+            Action confirmed,
+            Action dismissed)
+        {
+            AlertDialog.Builder builder = new AlertDialog.Builder(Mvx.Resolve<IMvxAndroidCurrentTopActivity>().Activity);
+            AlertDialog alertdialog = builder.Create();
+            builder.SetTitle(title);
+            builder.SetMessage(message);
+
+            builder.SetNegativeButton(dismissButtonTitle, (senderAlert, args) => {
+                if (dismissed != null)
+                {
+                    dismissed.Invoke();
+                }
+            });
+
+            builder.SetPositiveButton(okButtonTitle, (senderAlert, args) => {
+                if (confirmed != null)
+                {
+                    confirmed.Invoke();
+                }
+            });
+
+            builder.Show();
         }
     }
 
