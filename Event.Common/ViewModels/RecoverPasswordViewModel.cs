5e71febb1558a94af75b1ae19acd1a1f29848374
DIEGO ALEJANDRO ZAPATA VANEGAS <diegozapata176738@correo.itm.edu.co>
2019-06-04 18:09:41 -0500

    Recover password

--- Event.Common/ViewModels/RecoverPasswordViewModel.cs
+++ Event.Common/ViewModels/RecoverPasswordViewModel.cs
@@ -9,6 +9,8 @@
     using Newtonsoft.Json;
     using Services;
     using Event.Common.Interfaces;
+    using System;
+    using Event.Common.Helpers;
 
     public class RecoverPasswordViewModel : MvxViewModel
     {
@@ -16,6 +18,7 @@
         private readonly IApiService apiService;
         private readonly IMvxNavigationService navigationService;
         private readonly IDialogService dialogService;
+        private MvxCommand changePasswordCommand;
         private bool isEnabled;
         private bool isLoading;
         
@@ -29,6 +32,13 @@
             this.navigationService = navigationService;
             this.dialogService = dialogService;
             this.IsLoading = false;
+            this.IsEnabled = true;
+        }
+
+        public bool IsEnabled
+        {
+            get => this.isEnabled;
+            set => this.SetProperty(ref this.isEnabled, value);
         }
 
         public bool IsLoading
@@ -39,15 +49,59 @@
 
         public string Email { get; set; }
 
-        public ICommand ChangePasswordCommand
+        public ICommand RecoverpasswordCommand
         {
             get
             {
-                this.ChangePasswordCommand = this.ChangePasswordCommand ?? new MvxCommand(this.RegisterUser);
-                return this.ChangePasswordCommand;
+                this.changePasswordCommand = this.changePasswordCommand ?? new MvxCommand(this.RecoverPassword);
+                return this.changePasswordCommand;
             }
         }
 
+        private async void RecoverPassword()
+        {
+            if (string.IsNullOrEmpty(this.Email))
+            {
+                this.dialogService.Alert("Error", "You must enter an email.", "Accept");
+                return;
+            }
 
+            if (!RegexHelper.IsValidEmail(this.Email))
+            {
+                this.dialogService.Alert("Error", "You must enter a valid email.", "Accept");
+                return;
+            }
+
+            this.IsLoading = true;
+            this.IsEnabled = false;
+
+            var request = new RecoverPasswordRequest
+            {
+                Email = this.Email
+            };
+
+            var response = await this.apiService.RecoverPasswordAsync(
+                "https://diegozapataeventweb.azurewebsites.net",
+                "/api",
+                "/Account/RecoverPassword",
+                request);
+
+            this.IsLoading = false;
+            this.IsEnabled = true;
+
+            if (!response.IsSuccess)
+            {
+                this.dialogService.Alert("Error", response.Message, "Accept");
+                return;
+            }
+
+            this.dialogService.Alert(
+                "Ok",
+                "An email with instructions to change " +
+                "to change the password was sent",
+                "Accept",
+                () => { this.navigationService.Close(this); }
+            );
+        }
     }
 }
